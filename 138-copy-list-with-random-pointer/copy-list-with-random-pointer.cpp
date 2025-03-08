79b1a192e3464ee5563265c41d4cdaeed0752ea5
ary-an-sahu <aryansahu9021@gmail.com>
2025-03-08 16:39:32 +0530

    Time: 5 ms (65.29%) | Memory: 15 MB (71.89%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,23 +17,63 @@ public:
 class Solution {
 public:
 
-    Node* helper(Node* head, unordered_map<Node* , Node*> &mp){
-        if(head==0) return 0;
+    // Node* helperOld(Node* head, unordered_map<Node* , Node*> &mp){
+    //     if(head==0) return 0;
 
-        Node* newHead =  new Node(head->val);
-        mp[head] = newHead;
-        newHead->next = helper(head->next,mp);
+    //     Node* newHead =  new Node(head->val);
+    //     mp[head] = newHead;
+    //     newHead->next = helper(head->next,mp);
 
-        if(head->random!=NULL){
-            newHead-> random = mp[head->random];
-        }
-        return newHead;
+    //     if(head->random!=NULL){
+    //         newHead-> random = mp[head->random];
+    //     }
+    //     return newHead;
 
-    }
-
+    // }
+    
+   
 
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*> mp;  //old node ---> new node : mapping 
-        return helper(head,mp);
+        // unordered_map<Node*,Node*> mp;  //old node ---> new node : mapping 
+        // return helperOld(head,mp);
+
+        if(head==NULL)  return 0;
+
+        // Step 1: Clone A -> A'
+        Node * it = head;
+
+        while(it!= NULL){
+            Node* clonedNode =  new Node(it->val);
+            clonedNode->next = it->next;
+            it->next= clonedNode;
+            it = it->next->next;
+        }
+
+        // Step 2 - assign random links of A' with the help of A
+        it = head;
+
+        while(it!=NULL){
+
+            Node* clonedNode =  it->next;
+            clonedNode->random = it->random? it->random->next: nullptr;
+            it = it->next->next;
+        }
+
+        // Step 3 : detach A' from A-
+        it = head;
+
+        Node* clonedHead = it->next;
+        while(it){
+            Node* clonedNode = it->next;
+            it->next = it->next -> next;
+            if(clonedNode->next != NULL){
+                clonedNode -> next = clonedNode -> next -> next;
+            }
+            it = it->next;
+        }
+
+        return clonedHead;
+
+        // return helper(head);
     }
 };
