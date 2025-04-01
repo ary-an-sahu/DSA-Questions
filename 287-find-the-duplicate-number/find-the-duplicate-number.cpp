d52f22dcce09c1e31e64bfb05a98e07493d3354d
ary-an-sahu <aryansahu9021@gmail.com>
2025-04-01 13:04:52 +0530

    Time: 9 ms (47.15%) | Memory: 65.1 MB (69.06%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -27,23 +27,31 @@ public:
     // }
 
 
-    // negative index method - 
+    // negative index method -  here array is being modified 
+    // int findDuplicate(vector<int>& nums){
+
+    //     int ans = -1;
+
+    //     for(int i=0; i< nums.size();i++){
+
+    //         int index = abs(nums[i]);
+
+    //         if(nums[index]< 0){
+    //             ans = index;
+
+    //             break;
+    //         }
+
+    //         nums[index] *= -1;
+    //     }
+    //     return ans;
+    // }
+
+    // Positioning method - 
     int findDuplicate(vector<int>& nums){
-
-        int ans = -1;
-
-        for(int i=0; i< nums.size();i++){
-
-            int index = abs(nums[i]);
-
-            if(nums[index]< 0){
-                ans = index;
-
-                break;
-            }
-
-            nums[index] *= -1;
+        while(nums[0]!= nums[nums[0]]){
+            swap(nums[0], nums[nums[0]]);
         }
-        return ans;
+        return nums[0];
     }
 };
