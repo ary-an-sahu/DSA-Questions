4b3ea6cfc5730ae86b599cea83ec09ae69e8ee71
ary-an-sahu <aryansahu9021@gmail.com>
2025-04-01 12:57:05 +0530

    Time: 3 ms (68.38%) | Memory: 65 MB (90.07%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -13,15 +13,37 @@ public:
     //     return -1;
     // }
 
+
+    // array sorting method - 
+    // int findDuplicate(vector<int>& nums){
+    //     sort(nums.begin(), nums.end());
+
+    //     for(int i =0; i<nums.size()-1; i++){
+    //         if(nums[i] == nums[i+1]){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+    // negative index method - 
     int findDuplicate(vector<int>& nums){
-        sort(nums.begin(), nums.end());
 
-        for(int i =0; i<nums.size()-1; i++){
-            if(nums[i] == nums[i+1]){
-                return nums[i];
+        int ans = -1;
+
+        for(int i=0; i< nums.size();i++){
+
+            int index = abs(nums[i]);
+
+            if(nums[index]< 0){
+                ans = index;
+
+                break;
             }
-        }
-        return -1;
-    }
 
+            nums[index] *= -1;
+        }
+        return ans;
+    }
 };
