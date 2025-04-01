19f24edcdf10f45e425f71856107c912a02ca890
ary-an-sahu <aryansahu9021@gmail.com>
2025-04-01 11:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (43.10%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,20 +1,58 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int low = 0, mid = 0, high = nums.size()-1;
-        while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
+    // void sortColors(vector<int>& nums) {
+    //     int low = 0, mid = 0, high = nums.size()-1;
+    //     while(mid <= high){
+    //         if(nums[mid] == 0){
+    //             swap(nums[low], nums[mid]);
+    //             low++;
+    //             mid++;
+    //         }
+    //         else if(nums[mid] == 1){
+    //             mid++;
+    //         }
+    //         else{
+    //             swap(nums[mid], nums[high]);
+    //             high--;
+    //         }
+    //     }
+    // }
+
+    void sortColors(vector<int>& nums){
+        // 0-> red
+        // 1-> green
+        // 2-> blue
+        int countRed = 0;   
+        int countGreen =0;
+        int countBlue =0;
+
+        for(int i=0; i<nums.size();i++){
+            if(nums[i]==0){
+                countRed++;
             }
-            else if(nums[mid] == 1){
-                mid++;
+            else if(nums[i]== 1){
+                countGreen++;
             }
             else{
-                swap(nums[mid], nums[high]);
-                high--;
+                countBlue++;
             }
         }
+        int index = 0;
+        while(countRed--){
+            nums[index] = 0;
+            index++;
+        }
+
+        while(countGreen-- ){
+            nums[index] = 1;
+            index++;
+        }
+
+        while(countBlue-- ){
+            nums[index] = 2;
+            index++;
+        }
+
+
     }
 };
