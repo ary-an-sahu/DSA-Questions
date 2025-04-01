885555c36d2ac69e4d89c249404d541ca6b63cbb
ary-an-sahu <aryansahu9021@gmail.com>
2025-04-01 11:24:12 +0530

    Time: 8 ms (7.60%) | Memory: 113.7 MB (44.53%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    // double bruteforce(vector<int>& nums, int &k){
+    //     // complexity - O(n2)
+    //    int maxS = INT_MIN;
+    //    int i =0; 
+    //    int j = k-1;
+
+    //    while(j<nums.size()){
+    //         int sum = 0;
+    //         for(int t=i;t<=j; t++ ){
+    //             sum+=nums[t];
+    //         }
+    //         maxS = max(maxS, sum);
+    //         i++;
+    //         j++;
+
+    //    }
+    //    double maxAvg = maxS/(double)k;
+    //    return maxAvg;
+    // }
+
+    double slidingW(vector<int>& nums, int &k){
+       int i =0; 
+       int j = k-1;
+       int sum =0 ;
+
+       for(int t=i; t<=j;t++){
+        sum += nums[t];
+       }
+       int maxSum = sum;
+       j++;
+
+       while(j< nums.size()){
+
+        sum = sum - nums[i];
+        sum = sum + nums[j];
+        i++;
+        j++;
+        maxSum = max(maxSum, sum);
+       }
+
+       double maxAvg = maxSum /(double) k;
+       return maxAvg;
+    }
+
+    
+    double findMaxAverage(vector<int>& nums, int k){
+
+        // return bruteforce(nums,k);
+        return slidingW(nums,k);
+    }
+       
+};
