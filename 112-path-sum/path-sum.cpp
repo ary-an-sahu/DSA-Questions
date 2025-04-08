eaa58c17be47576cfe965152f340caa248e7838c
ary-an-sahu <aryansahu9021@gmail.com>
2025-04-08 15:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (34.57%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,10 +11,45 @@
  */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int sum) {
-        if(!root)return false;                                         //Terminating Condition
-        sum=sum-root->val;                                             //Body
-        if(sum==0&&!root->left&&!root->right)return true;              //body
-        return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);//Propagation
+    // bool hasPathSum(TreeNode* root, int sum) {
+    //     if(!root)return false;                                         //Terminating Condition
+    //     sum=sum-root->val;                                             //Body
+    //     if(sum==0&&!root->left&&!root->right)return true;              //body
+    //     return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);//Propagation
+    // }
+
+    bool solve(TreeNode* root, int tar , int sum){
+        if(root == NULL){
+            return false;
+        }
+
+        sum += root->val;
+
+        if(root->left == NULL && root->right == NULL){ //leaf node pr aa gya
+            if(sum == tar){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        bool left = solve(root->left, tar, sum);
+        bool right = solve(root->right,tar,sum);
+
+        return left || right;
+       
+    }
+
+    bool hasPathSum(TreeNode* root, int target){
+
+        if(root == NULL ){
+            return false;
+        }
+        int sum = 0;
+        
+        bool ans = solve(root, target, sum);
+        return ans;
+        
     }
 };
