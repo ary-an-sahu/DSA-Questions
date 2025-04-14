ba122acd8cea47ea1014b297381e82dfa2b57e2a
ary-an-sahu <aryansahu9021@gmail.com>
2025-04-14 20:34:14 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (99.59%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -1,42 +1,63 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int getLength(TreeNode* root){
-        if(root == NULL){
-            return 0;
+    /**
+    * Definition for a binary tree node.
+    * struct TreeNode {
+    *     int val;
+    *     TreeNode *left;
+    *     TreeNode *right;
+    *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    * };
+    */
+    class Solution {
+    public:
+        // int getLength(TreeNode* root){
+        //     if(root == NULL){
+        //         return 0;
+        //     }
+
+        //     int left = getLength(root->left);
+        //     int right =  getLength(root->right);
+        //     int maxLength = max(left, right);
+        //     int totalLength = maxLength +1;
+        //     return totalLength;
+            
+        // }
+
+
+        // int diameterOfBinaryTree(TreeNode* root) {
+        //     //base case - 
+        //     if(root == NULL){
+        //         return 0;
+        //     }
+
+        //     int opt1 = diameterOfBinaryTree(root->left);
+        //     int opt2 = diameterOfBinaryTree(root->right);
+        //     int opt3 = getLength(root->left) + getLength(root->right);
+
+        //     int maxDia = max(opt1,max(opt2,opt3));
+
+        //     return maxDia;
+        // }
+
+        int D = 0; //global variable
+
+        int height(TreeNode* root){
+            if(root==NULL){
+                return 0;
+            }
+
+            int lh = height(root->left);
+            int rh = height(root->right);
+
+            int currD = lh + rh;
+            D = max(D,currD);
+
+            return max(lh,rh)+1;
         }
 
-        int left = getLength(root->left);
-        int right =  getLength(root->right);
-        int maxLength = max(left, right);
-        int totalLength = maxLength +1;
-        return totalLength;
-        
-    }
-
-
-    int diameterOfBinaryTree(TreeNode* root) {
-        //base case - 
-        if(root == NULL){
-            return 0;
+        int diameterOfBinaryTree(TreeNode* root) {
+            height(root);
+            return D;
         }
-
-        int opt1 = diameterOfBinaryTree(root->left);
-        int opt2 = diameterOfBinaryTree(root->right);
-        int opt3 = getLength(root->left) + getLength(root->right);
-
-        int maxDia = max(opt1,max(opt2,opt3));
-
-        return maxDia;
-    }
-};
+    };
