ed64f687f28b09a2001bae13e6a8cba8c85b5cfd
ary-an-sahu <aryansahu9021@gmail.com>
2025-04-14 20:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (57.54%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,50 +9,77 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
+    class Solution {
+    public:
 
-    int getHeight(TreeNode* root){
-        if(root== NULL){
-            return 0;
+
+        //slow approach - O(n2) => 
+
+        // int getHeight(TreeNode* root){
+        //     if(root== NULL){
+        //         return 0;
+        //     }
+
+        //     int left = getHeight(root->left);
+        //     int right = getHeight(root->right);
+        //     int maxH = max(left, right);
+        //     int totalH = maxH +1;
+
+        //     return totalH;
+        // }
+        // bool isBalanced(TreeNode* root) {
+            
+        //     if(root == NULL){
+        //         return true;
+        //     }
+
+        //     //ek case hum solve krenge 
+        //     int leftSubtreeH = getHeight(root->left);
+        //     int rightSubtreeH = getHeight(root->right);
+        //     int absDiff = abs(leftSubtreeH - rightSubtreeH);
+        //     bool status = absDiff <= 1;
+
+        //     // baaki recursion sambhal lega 
+        //     bool leftAns = isBalanced(root->left);
+        //     bool rightAns = isBalanced(root->right);
+
+        //     // i can say that whether tree is balancerd or not when?
+        //     // curr node balanced
+        //     // left subtree balanced
+        //     // right subtree balanced
+
+
+        //     if(status && leftAns && rightAns ){
+        //         return true;
+        //     }
+
+        //     else{
+        //         return false;
+        //     }
+
+        // }
+
+        bool isBalancedT = true; //global variable
+
+        int height(TreeNode* root){
+            if(root==NULL){
+                return 0;
+                
+            }
+
+            int lh = height(root->left);
+            int rh = height(root->right);
+
+            if(isBalancedT && abs(lh-rh) >1){
+                isBalancedT = false;
+            }
+
+            return max(lh,rh)+1;
+
         }
 
-        int left = getHeight(root->left);
-        int right = getHeight(root->right);
-        int maxH = max(left, right);
-        int totalH = maxH +1;
-
-        return totalH;
-    }
-    bool isBalanced(TreeNode* root) {
-        
-        if(root == NULL){
-            return true;
+        bool isBalanced(TreeNode* root) {
+            height(root);
+            return isBalancedT;
         }
-
-        //ek case hum solve krenge 
-        int leftSubtreeH = getHeight(root->left);
-        int rightSubtreeH = getHeight(root->right);
-        int absDiff = abs(leftSubtreeH - rightSubtreeH);
-        bool status = absDiff <= 1;
-
-        // baaki recursion sambhal lega 
-        bool leftAns = isBalanced(root->left);
-        bool rightAns = isBalanced(root->right);
-
-        // i can say that whether tree is balancerd or not when?
-        // curr node balanced
-        // left subtree balanced
-        // right subtree balanced
-
-
-        if(status && leftAns && rightAns ){
-            return true;
-        }
-
-        else{
-            return false;
-        }
-
-    }
-};
+    };
