1921e9a5a1da20cfa9646218c5d73b6ac333aefa
ary-an-sahu <aryansahu9021@gmail.com>
2025-04-26 18:58:19 +0530

    Time: 99 ms (5.13%), Space: 301.7 MB (5.03%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -10,26 +10,45 @@
  */
 class Solution {
 public:
-    ListNode* mergeNodes(ListNode* head) {
-        ListNode* modify = head->next; // Start from the node after the initial 0
-        ListNode* nextSum = modify;
+    // ListNode* mergeNodes(ListNode* head) {
+    //     ListNode* modify = head->next; // Start from the node after the initial 0
+    //     ListNode* nextSum = modify;
 
-        while (nextSum != nullptr) {
-            int sum = 0;
-            // Find the sum of all nodes until you encounter a 0.
-            while (nextSum->val != 0) {
-                sum += nextSum->val;
-                nextSum = nextSum->next;
-            }
+    //     while (nextSum != nullptr) {
+    //         int sum = 0;
+    //         // Find the sum of all nodes until you encounter a 0.
+    //         while (nextSum->val != 0) {
+    //             sum += nextSum->val;
+    //             nextSum = nextSum->next;
+    //         }
 
-            // Assign the sum to the current node's value.
-            modify->val = sum;
-            // Move nextSum to the first non-zero value of the next block.
-            nextSum = nextSum->next;
-            // Move modify also to this node.
-            modify->next = nextSum;
-            modify = modify->next;
+    //         // Assign the sum to the current node's value.
+    //         modify->val = sum;
+    //         // Move nextSum to the first non-zero value of the next block.
+    //         nextSum = nextSum->next;
+    //         // Move modify also to this node.
+    //         modify->next = nextSum;
+    //         modify = modify->next;
+    //     }
+    //     return head->next; // Skip the initial 0 node.
+    // }
+
+    ListNode* mergeNodes(ListNode* head){
+
+        ListNode* temp  = head->next;
+        int sum = 0;
+
+        if(temp == NULL){
+            return NULL;
         }
-        return head->next; // Skip the initial 0 node.
+
+        while(temp->val != 0){
+            sum = sum+ temp->val;
+            temp = temp->next;
+        }
+        ListNode* newNode = new ListNode(sum);
+
+        newNode->next = mergeNodes(temp);
+        return newNode;
     }
 };
