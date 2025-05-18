7634cda0ec23ed01047a5e155673cbbad3d6ae1e
ary-an-sahu <aryansahu9021@gmail.com>
2025-05-18 18:09:31 +0530

    Time: 24 ms (44.64%), Space: 44.6 MB (37.14%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -10,29 +10,35 @@
  * };
  */
 class Solution {
+    
 public:
-    vector<TreeNode*> ans;
-    unordered_map<string, int> subTreeMap;
 
-    string preorder(TreeNode* root){
-        if(!root) return "N";
+
+    vector<TreeNode*> ans;
+    unordered_map<string,int> subTreeMap;
+
+    string  preorder(TreeNode* root){
+
+        if(!root ) return "N";
+
         string curr = to_string(root->val);
         string l = preorder(root->left);
         string r = preorder(root->right);
 
-        string s = curr + "," + l + "," + r;
+        string s = curr+","+ l +","+ r;
 
-        if(subTreeMap.find(s) != subTreeMap.end()){
+        if(subTreeMap.find(s)!= subTreeMap.end()){
+
             if(subTreeMap[s] == 1){
                 ans.push_back(root);
             }
-            subTreeMap[s] ++;   
+            subTreeMap[s]++;
         }
-       else{
+        else{
             subTreeMap[s] = 1;
         }
-        return s;
 
+        return s;
     }
 
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
