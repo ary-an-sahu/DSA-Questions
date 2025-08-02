0a8926dced8d13b496f021197e2ccf8d884ee243
ary-an-sahu <aryansahu9021@gmail.com>
2025-08-02 19:53:35 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.54%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        //Kadane's Algorithm
+        int maxSum = INT_MIN, sum =0;
+
+        for(int i=0 ; i<nums.size();i++){
+
+            sum+= nums[i];
+
+
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+
+            if(sum < 0){
+                sum =0;
+            }
+        }
+        return maxSum;
+    }
+};
