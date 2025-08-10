679f456e30130decb3689932d4fcc310a4479e17
ary-an-sahu <aryansahu9021@gmail.com>
2025-08-11 01:34:38 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.08%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,52 @@
 class Solution {
 public:
-//Brute force like solution
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int sIndex = -1;
-        int eIndex = -1;
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == target){
-                if(sIndex == -1){
-                    sIndex = i;
-                }
-                eIndex = i;
+    int firstOccur(vector<int>& nums, int target, int &firstOcc) {
+        int s = 0, e = nums.size() - 1;
+        
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] == target) {
+                firstOcc = mid;
+                e = mid - 1; // keep searching left for earlier occurrence
+            }
+            else if (nums[mid] < target) {
+                s = mid + 1; // move right
+            } 
+            else {
+                e = mid - 1; // move left
             }
         }
+        return firstOcc;
+    }
 
-        return {sIndex, eIndex};
+    int lastOccur(vector<int>& nums, int target, int &lastOcc) {
+        int s = 0, e = nums.size() - 1;
+        
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
 
+            if (nums[mid] == target) {
+                lastOcc = mid;
+                s = mid + 1; // keep searching right for later occurrence
+            }
+            else if (nums[mid] < target) {
+                s = mid + 1; // move right
+            } 
+            else {
+                e = mid - 1; // move left
+            }
+        }
+        return lastOcc;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int firstOcc = -1, lastOcc = -1;
+
+        firstOccur(nums, target, firstOcc);
+        lastOccur(nums, target, lastOcc);
+
+        return {firstOcc, lastOcc};
     }
 };
