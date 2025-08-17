496fb551e86bb1d13e3de92c595d622e8eed4c83
ary-an-sahu <aryansahu9021@gmail.com>
2025-08-18 01:36:31 +0530

    Time: 116 ms (9.24%), Space: 18.7 MB (67.31%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
-    // Bruteforce -
+    bool searchElement(vector<int>&arr , int n , int target){
+
+        int low = 0;
+        int high = n -1;
+
+        while(low <= high){
+            int mid = (low+high)/2;
+
+            if(arr[mid] == target){
+                return true;
+            }
+            
+            if(arr[mid]> target){
+                high = mid -1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int rows = matrix.size();
         int cols = matrix[0].size();
+        bool finalAns = false;
 
         for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
-            }
+            
+            bool ans = searchElement(matrix[i], cols , target);
+
+            finalAns = finalAns || ans;
         }
 
-        return false;
+        return finalAns;
     }
 };
