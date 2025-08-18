1d42fcacdf9e8500e1f575cedf96bb2a5be52c87
ary-an-sahu <aryansahu9021@gmail.com>
2025-08-18 16:12:56 +0530

    Time: 20 ms (5.05%), Space: 12.2 MB (20.38%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        stringstream ss(s);
-        string word;
-        vector<string> words;
+    string trim(const string &s) {
+        int start = s.find_first_not_of(" \t\n\r\f\v");
+        if (start == string::npos) return "";
+        int end = s.find_last_not_of(" \t\n\r\f\v");
+        return s.substr(start, end - start + 1);
+    }
 
-        // Extract words (ignores multiple spaces automatically)
-        while (ss >> word) {
-            words.push_back(word);
+    string reverseWords(string s) {
+        string newS = trim(s);
+        int size = newS.length();
+        vector<string> store;
+        int start = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (newS[i] == ' ') {
+                if (i > start) { // avoid multiple spaces
+                    string word = newS.substr(start, i - start);
+                    store.insert(store.begin(), word); // insert at front
+                }
+                start = i + 1;
+            }
         }
 
-        // Reverse words
-        reverse(words.begin(), words.end());
+        // Add last word
+        if (start < size) {
+            string word = newS.substr(start, size - start);
+            store.insert(store.begin(), word);
+        }
 
         // Join words with space
-        string result;
-        for (int i = 0; i < words.size(); i++) {
-            result += words[i];
-            if (i != words.size() - 1) result += " ";
+        string final = "";
+        for (int i = 0; i < store.size(); i++) {
+            final += store[i];
+            if (i != store.size() - 1) final += " ";
         }
 
-        return result;
+        return final;
     }
 };
