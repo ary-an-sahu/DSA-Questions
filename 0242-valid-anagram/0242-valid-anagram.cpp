055f2b38e4ac243b7d0a3f1e10105bb60af29b6e
ary-an-sahu <aryansahu9021@gmail.com>
2025-08-20 01:32:10 +0530

    Time: 2 ms (56.92%), Space: 9.9 MB (26.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(t.begin(),t.end());
-        sort(s.begin(),s.end());
+        unordered_map<char,int>mps;
+        unordered_map<char,int>mpt;
+        int n = s.size();
+        int m = t.size();
 
+        if(n != m)return false;
 
-        if(s == t){
-            return true;
+        for(int i=0; i<n;i++){
+            mps[s[i]]++;
+            mpt[t[i]]++;
+        }  
+
+        for(int i=0; i<m ;i++){
+            if(mps[s[i]] != mpt[s[i]]){
+                return false;
+            }
         }
 
-        return false;
+        return true;
+
+         
     }
 };
