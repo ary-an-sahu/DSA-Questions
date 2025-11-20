9a6dd4ebdf23698dac30d237ae3f24a91bb5f4ba
ary-an-sahu <aryansahu9021@gmail.com>
2025-11-20 19:58:26 +0530

    Time: 0 ms (100%), Space: 10.8 MB (87.52%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,57 +11,39 @@
  */
 class Solution {
 public:
-    // vector<int>ans;
-    // void inorder(TreeNode* root){
-    //     if(root==nullptr){
-    //         return;
-    //     }
-    //     inorder(root->left);
-    //     ans.push_back(root->val);
-    //     inorder(root->right);
-    // }
-    // vector<int> inorderTraversal(TreeNode* root) {
-    //     inorder(root);
-    //     return ans;
-    // }
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int> inorder;
 
+        TreeNode* cur = root;
 
-    // Using Morris traversal -  
-    vector<int> inorderTraversal(TreeNode* root){
-        vector<int> ans;
+        while(cur!= NULL){
 
-        TreeNode* curr = root;
-        while(curr){
-
-            //if curr ka left is null then visit it and go right
-            if(curr->left == nullptr ){
-                ans.push_back(curr->val);
-                curr = curr -> right;
+            if(cur->left == NULL){
+                inorder.push_back(cur->val);
+                cur = cur->right;
             }
-
-            // left node is not null 
             else{
-                //find  inorder predecessor
-                TreeNode* pred = curr->left;
-                while(pred->right!= curr && pred->right){
-                    pred = pred -> right;
+
+                TreeNode* prev = cur->left;
+
+                while(prev->right && prev->right != cur){
+                    prev = prev ->right;
                 }
 
-                // if pred right node is NULL, go left after establishing link from pred to curr;
-                if(pred->right == nullptr){
-                    pred->right = curr;
-                    curr = curr -> left;
+                if(prev->right == NULL){
+                    prev -> right = cur;
+                    cur = cur ->left;
                 }
                 else{
-                    // if right node is not null 
-                    // left is already visited go right after visiting curr node,while removing the link            
-                    pred-> right = nullptr;
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                }
 
+                    prev->right = NULL;
+                    inorder.push_back(cur->val);
+                    cur = cur ->right;
+                }
             }
         }
-        return ans;
+
+        return inorder;
     }
 };
