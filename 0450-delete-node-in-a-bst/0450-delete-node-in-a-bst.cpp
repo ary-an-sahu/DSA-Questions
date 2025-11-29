9e69485fbd12cbea3ae261642bdb69ef2bc3f364
ary-an-sahu <aryansahu9021@gmail.com>
2025-11-29 15:22:34 +0530

    Time: 0 ms (100%), Space: 34.2 MB (72.91%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -6,65 +6,72 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+
     TreeNode* helper(TreeNode* root){
 
         if(root->left == NULL){
-            return root ->right;
+            return root->right;
         }
-
         else if(root->right == NULL){
             return root->left;
         }
 
-        TreeNode* rightChild = root ->right;
+        TreeNode* rightChild = root->right;
         TreeNode* lastRight = findLastRight(root->left);
 
         lastRight ->right = rightChild;
         return root->left;
+
     }
 
     TreeNode* findLastRight(TreeNode* root){
-        if(root->right == NULL){
+        if(root-> right == NULL){
             return root;
         }
         return findLastRight(root->right);
     }
 
     TreeNode* deleteNode(TreeNode* root, int key) {
-        
-        if(root == NULL) return NULL;
 
-        if(root->val == key){
+        if (root == NULL) {
+            return NULL;
+        }
+
+        if (root->val == key) {
             return helper(root);
         }
 
         TreeNode* dummy = root;
 
-        while(root!= NULL){
-            if(root->val > key){
+        while (root != NULL) {
 
-                if(root->left != NULL && root->left->val == key){
+            if (root->val > key) {
+                if (root->left != NULL && root->left->val == key) {
                     root->left = helper(root->left);
                     break;
                 }
-                else{
-                    root= root->left;
+
+                else {
+                    root = root->left;
                 }
-            }else{
-                if(root->right != NULL && root->right -> val == key){
-                    root -> right = helper(root->right);
+            }
+
+            else {
+                if (root->right != NULL && root->right->val == key) {
+                    root->right = helper(root->right);
                     break;
-                }
-                else{
+                } else {
                     root = root->right;
                 }
             }
         }
+
         return dummy;
     }
 };
