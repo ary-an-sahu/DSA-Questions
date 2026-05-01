8d6b9a1ec02d9ba57d74046767c58e1645157d16
ary-an-sahu <aryansahu9021@gmail.com>
2026-05-02 00:49:53 +0530

    Time: 2 ms (15.64%), Space: 25.5 MB (5.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,41 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;  // pointer to place non-zero element
-        for (int j = 0; j < nums.size(); j++) {
-            if (nums[j] != 0) {
-                swap(nums[i], nums[j]);
-                i++;
+
+        if(nums.size() == 0){
+            return;
+        }
+
+        
+        vector<int>store;
+        vector<int>cnt0;
+        int cnt = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]== 0){
+                cnt0.push_back(nums[i]);
+            }
+
+            else{
+                store.push_back(nums[i]);
             }
         }
+
+
+        for(int j = 0; j<cnt0.size() ; j++){
+
+            store.push_back(cnt0[j]);
+        }
+
+
+        for(int k = 0; k< store.size() ; k++){
+
+            nums[k] = store[k];
+        }
+
+
+
+        
+
+
     }
 };
