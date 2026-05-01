dc3d49e94020a85f3e6c2c7d089fcc07427b8e21
ary-an-sahu <aryansahu9021@gmail.com>
2026-05-02 00:59:02 +0530

    Time: 0 ms (100%), Space: 23.9 MB (18.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
-        if(nums.size() == 0){
-            return;
-        }
-
         
-        vector<int>store;
-        vector<int>cnt0;
-        int cnt = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]== 0){
-                cnt0.push_back(nums[i]);
-            }
+        int i = 0;
 
-            else{
-                store.push_back(nums[i]);
+        for(int j = 0; j<nums.size(); j++){
+
+            if(nums[j] != 0){
+
+                swap(nums[i], nums[j]);
+                i++;
             }
         }
 
-
-        for(int j = 0; j<cnt0.size() ; j++){
-
-            store.push_back(cnt0[j]);
-        }
-
-
-        for(int k = 0; k< store.size() ; k++){
-
-            nums[k] = store[k];
-        }
-
-
-
         
-
-
     }
 };
