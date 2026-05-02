ac37505e858671e4f1af4a1d31d57fdb320104f5
ary-an-sahu <aryansahu9021@gmail.com>
2026-05-03 02:42:47 +0530

    Time: 49 ms (43.72%), Space: 46.8 MB (30.95%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mpp;
-        mpp[0] =1;
-        int preSum = 0;
-        int count = 0;
 
-        for(int i=0 ; i<nums.size(); i++){
-            preSum += nums[i];
-            int remove = preSum - k;
-            count += mpp[remove];
-            mpp[preSum] += 1;
+        //prefix sum concept
+        int n = nums.size();
+        int cnt = 0;
+        vector<int>prefixSum(n);
+        
+        prefixSum[0] = nums[0];
+
+        for(int i=1; i<n; i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+        
+        unordered_map<int,int>mpp; // ps, count
+        for(int i = 0; i<n ; i++){
+
+            if(prefixSum[i] == k){
+                cnt++;
+            }
+
+            int val = prefixSum[i] - k;
+
+            if(mpp.find(val) != mpp.end()){
+                cnt+= mpp[val];
+            }
+
+            if(mpp.find(prefixSum[i]) == mpp.end()){
+                mpp[prefixSum[i]] = 0;
+            }
+
+            mpp[prefixSum[i]] ++;
         }
 
-        return count;
-
+        return cnt;
     }
 };
