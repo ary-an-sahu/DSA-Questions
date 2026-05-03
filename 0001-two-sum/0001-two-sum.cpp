1d04c86478571608c9532f939340149c308bfa28
ary-an-sahu <aryansahu9021@gmail.com>
2026-05-04 01:25:08 +0530

    Time: 9 ms (37.9%), Space: 16.1 MB (5.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         unordered_map<int,int>mpp;
-        unordered_map<int,bool>visited;
+
         
         for(int i=0; i<nums.size();i++){
 
