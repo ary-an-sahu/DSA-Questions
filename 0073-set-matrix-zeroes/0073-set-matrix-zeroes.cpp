a18fdf3b839a729e7e2d8ec88622d3b44b7abbb0
ary-an-sahu <aryansahu9021@gmail.com>
2026-05-08 22:12:31 +0530

    Time: 35 ms (6.52%), Space: 24.2 MB (5.03%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,37 +1,55 @@
 class Solution {
 public:
-    
-    //additional pair is required for this approach ->better solution 
+
+    void makeZero(vector<vector<int>>&matrix , int i , int j){
+        
+        int r = matrix.size();
+        int c = matrix[0].size();
+
+        
+        //make row zero
+        for(int k = 0 ; k<c ; k++){
+
+            matrix[i][k] = 0;
+        }
+
+        //make column zero
+
+        for(int k = 0; k<r ; k++){
+
+            matrix[k][j] = 0;
+        }
+        
+    }
+
     void setZeroes(vector<vector<int>>& matrix) {
-        int r = matrix.size(); //rows
-        int c = matrix[0].size(); //columns
 
-        vector<pair<int , int>> store;
+        set<pair<int,int>>st;
+        int r = matrix.size();
+        int c = matrix[0].size();
 
-        for(int i=0; i<r ;i++){
-            for(int j =0; j<c ; j++){
+        if(r == 0 && c == 0){
+            return;
+        }
+
+
+        for(int i=0; i<r ; i++){
+
+            for(int j = 0 ; j<c ; j++){
+
                 if(matrix[i][j] == 0){
-                    store.push_back({i,j});
+                    st.insert({i,j});
                 }
-
-            }
-
-        }
-
-        for(auto it: store){
-
-            int i= it.first;
-            int j = it.second;
-
-            for(int m =0; m<c; m++){
-                matrix[i][m] = 0;
-
-            }
-            for(int n =0; n<r ; n++){
-                matrix[n][j] = 0;
             }
         }
 
+        for(auto it: st){
 
+            makeZero(matrix,it.first, it.second);
+        }
+        
+
+
+        
     }
 };
